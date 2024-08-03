a0e1bb08c837e2a6b031b410eef86d6ec2722293
Diebbo <diego.barbieri03@libero.it>
2024-08-03 17:00:28 +0200

    Login coi contro cazzi

--- selfie-client/src/App.tsx
+++ selfie-client/src/App.tsx
@@ -4,9 +4,7 @@ import {Button} from "@nextui-org/button";
 function App() {
   // 2. Wrap NextUIProvider at the root of your app
   return (
-    <NextUIProvider>
         <Button className="dark">Click me</Button>
-    </NextUIProvider>
   );
 }
 
