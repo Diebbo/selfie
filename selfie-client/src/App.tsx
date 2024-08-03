e92ee61b339fb53d7397b8f027ec76ce758e3c37
Diebbo <diego.barbieri03@libero.it>
2024-08-03 15:46:12 +0200

    implemented nextui test

--- selfie-client/src/App.tsx
+++ selfie-client/src/App.tsx
@@ -1,36 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import './App.css'
+import {NextUIProvider} from "@nextui-org/system";
+import {Button} from "@nextui-org/button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // 2. Wrap NextUIProvider at the root of your app
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <NextUIProvider>
+        <Button className="dark">Click me</Button>
+    </NextUIProvider>
+  );
 }
 
 export default App
