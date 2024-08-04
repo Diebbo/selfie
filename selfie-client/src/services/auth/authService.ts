876f9b0f207ca42426de70f83ce692e47a180a49
Diebbo <diego.barbieri03@libero.it>
2024-08-04 16:06:36 +0200

    adding login Interfaces

--- selfie-client/src/services/auth/authService.ts
+++ selfie-client/src/services/auth/authService.ts
@@ -1,19 +1,38 @@
 // Purpose: Handles the login process.
+import { Login, Register } from "../../models/auth/register";
 
 // Purpose: Handles the login process.
-export async function login(username: string, password: string) {
-  const response = await fetch("/api/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, password }),
-		credentials: 'include',
-  });
+export async function login(user: Login) {
+	const response = await fetch("/api/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(user),
+		credentials: "include",
+	});
 
-  if (!response.ok) {
-    throw new Error("Login failed");
-  }
+	if (!response.ok) {
+		const error = await response.json();
+		throw new Error(error.message || "Login failed");
+	}
 
-  return response.json();
+	return response.json();
+}
+
+export async function register(user: Register) {
+	const response = await fetch("/api/register", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(user),
+	});
+
+	if (!response.ok) {
+		const error = await response.json();
+		throw new Error(error.message || "Registration failed");
+	}
+
+	return response.json();
 }
