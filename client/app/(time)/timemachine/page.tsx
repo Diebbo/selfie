ae9a99f5d6be410b96eef3a34283170b639d6206
Diebbo <diego.barbieri03@libero.it>
2024-08-13 19:18:21 +0200

    fixed time machine form

--- client/app/(time)/timemachine/page.tsx
+++ client/app/(time)/timemachine/page.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import TimeModifierClient from '@/components/timemachine/content';
-import { cookies } from 'next/headers';
 import { changeCurrentTime } from '@/actions/setTime';
 import '@/styles/globals.css';
 
-async function handleTimeChange() {
-  'use server'
-	const formData = new FormData();
-  const time = formData.get('time') as string;
-  const cookieStore = cookies();
-  const token = cookieStore.get('token')?.value;
+async function handleTimeChange(formData: FormData) : Promise<{ success: boolean, error?: string }>{
+	'use server'
+	const time = formData.get('time') as string;
 
-  if (!token) {
-    throw new Error('Not authenticated');
-  }
-
-  try {
-    await changeCurrentTime(new Date(time));
-    return { success: true };
-  } catch (error: any) {
-    return { success: false, error: error.message };
-  }
+	try {
+		await changeCurrentTime(new Date(time));
+		return { success: true };
+	} catch (error: any) {
+		console.log(error);
+		return { success: false, error: error.message };
+	}
 }
 
 export default function TimeMachine() {
-  return <TimeModifierClient onSubmit={handleTimeChange} />;
+	return <TimeModifierClient onSubmit={handleTimeChange} />;
 }
 
