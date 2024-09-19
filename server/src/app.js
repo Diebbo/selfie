920e55bafef5d740a58306e19975877336e053b4
diego <diego.barbieri03@libero.it>
2024-09-19 19:30:40 +0200

    levata roba inutile

--- server/src/app.js
+++ server/src/app.js
@@ -22,10 +22,6 @@ export function createApp({ dirpath, database }) {
 
   const app = express();
 
-  // middelewares
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
   app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
   app.locals.pluralize = pluralize;
@@ -42,22 +38,6 @@ export function createApp({ dirpath, database }) {
   app.use("/api/config", createTimeRouter(database));
   app.use("/api/calendar", createCalendarRouter(database));
   app.use("/api/notes", createNoteRouter(database));
-
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    next(createError(404));
-  });
-
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-  });
 
   return app;
 }
