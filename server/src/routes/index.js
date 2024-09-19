eab00a9b3f6960839837311ab2afe6f9e9f2e107
diego <diego.barbieri03@libero.it>
2024-09-19 19:31:18 +0200

    getNotes createNotes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -10,18 +10,5 @@ router.get("/ready", function (req, res) {
   res.send("Server Runnign!!");
 });
 
-router.get("/dashboard", cookieJwtAuth, function (req, res) {
-  // sending the response
-  console.log(req.user);
-  if(req.user.role === "admin") {
-    res.send("Welcome to the admin dashboard");
-  }
-  if(req.user.role === "user") {
-    res.send("Welcome to the dashboard");
-  }
-  
-  res.send("You are not authorized to view this page");
-});
-
 export default router;
 
