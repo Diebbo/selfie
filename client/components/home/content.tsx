0c26e6313c38e47eeb4854794f2c4a8938df9e00
Diebbo <diego.barbieri03@libero.it>
2024-09-27 17:29:30 +0200

    new page section

--- client/components/home/content.tsx
+++ client/components/home/content.tsx
@@ -2,9 +2,6 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import { TableWrapper } from "../table/table";
-import { CardBalance1 } from "./card-balance1";
-import { CardBalance2 } from "./card-balance2";
-import { CardBalance3 } from "./card-balance3";
 import { EventCard } from "./event-card";
 import { CardAgents } from "./card-agents";
 import { CardTransactions } from "./card-transactions";
@@ -12,6 +9,7 @@ import { Link } from "@nextui-org/react";
 import NextLink from "next/link";
 import { SelfieEvent } from "@/helpers/types";
 import dummy from "@/helpers/dummyvar";
+import { CardChats } from "./card-chats";
 
 const Chart = dynamic(
   () => import("../charts/steam").then((mod) => mod.Steam),
@@ -20,17 +18,26 @@ const Chart = dynamic(
   }
 );
 
-export const Content = (props: any) => (
+interface ContentProps {
+  events: {
+    createdEvents: any[];
+    participatingEvents: any[];
+  };
+  chats: any[];
+}
+
+export const Content = (props: ContentProps) => (
   <div className="h-full lg:px-6">
     <div className="flex justify-center gap-4 xl:gap-6 pt-3 px-4 lg:px-0  flex-wrap xl:flex-nowrap sm:pt-10 max-w-[90rem] mx-auto w-full">
       <div className="mt-6 gap-6 flex flex-col w-full">
         {/* Card Section Top */}
         <div className="flex flex-col gap-2">
           <h3 className="text-xl font-semibold">Next Events</h3>
+          <h4 className="text-l font-semibold">Your Events</h4>
           <div className="grid md:grid-cols-2 grid-cols-1 2xl:grid-cols-3 gap-5  justify-center w-full">
             {
-              Array.isArray(props.events) && props.events.length > 0 ? (
-                props.events.map((event: SelfieEvent, index: number) => (
+              Array.isArray(props.events.createdEvents) && props.events.createdEvents.length > 0 ? (
+                props.events.createdEvents.slice(0,5).map((event: SelfieEvent, index: number) => (
                   <EventCard data={event} theme={index} key={index} />
                 ))
               ) : (
@@ -44,42 +51,54 @@ export const Content = (props: any) => (
             <CardBalance2 />
             <CardBalance3 /> */}
           </div>
+          <h4 className="text-l font-semibold">Participating Events</h4>
+          <div className="grid md:grid-cols-2 grid-cols-1 2xl:grid-cols-3 gap-5  justify-center w-full">
+            {
+              Array.isArray(props.events.participatingEvents) && props.events.participatingEvents.length > 0 ? (
+                props.events.participatingEvents.slice(0,5).map((event: SelfieEvent, index: number) => (
+                  <EventCard data={event} theme={index} key={index} />
+                ))
+              ) : (
+                <div className="text-success">Niente attivita&apos; sociali</div> // Your alternative content here
+              )
+            }
+          </div>
         </div>
 
-        {/* Chart */}
-        <div className="h-full flex flex-col gap-2">
-          <h3 className="text-xl font-semibold">Calendar</h3>
-          <div className="w-full bg-default-50 shadow-lg rounded-2xl p-6 ">
-            {/* <Chart /> */}
-            TODO
+          {/* Chart */}
+          <div className="h-full flex flex-col gap-2">
+            <h3 className="text-xl font-semibold">Calendar</h3>
+            <div className="w-full bg-default-50 shadow-lg rounded-2xl p-6 ">
+              {/* <Chart /> */}
+              TODO
+            </div>
+          </div>
+        </div>
+
+        {/* Left Section */}
+        <div className="mt-4 gap-2 flex flex-col xl:max-w-md w-full">
+          <h3 className="text-xl font-semibold">Friends</h3>
+          <div className="flex flex-col justify-center gap-4 flex-wrap md:flex-nowrap md:flex-col">
+            <CardAgents />
+            <CardChats chats={props.chats} />
           </div>
         </div>
       </div>
 
-      {/* Left Section */}
-      <div className="mt-4 gap-2 flex flex-col xl:max-w-md w-full">
-        <h3 className="text-xl font-semibold">Friends</h3>
-        <div className="flex flex-col justify-center gap-4 flex-wrap md:flex-nowrap md:flex-col">
-          <CardAgents />
-          <CardTransactions />
+      {/* Table Latest Users */}
+      <div className="flex flex-col justify-center w-full py-5 px-4 lg:px-0  max-w-[90rem] mx-auto gap-3">
+        <div className="flex  flex-wrap justify-between">
+          <h3 className="text-center text-xl font-semibold">Latest Users</h3>
+          <Link
+            href="/accounts"
+            as={NextLink}
+            color="primary"
+            className="cursor-pointer"
+          >
+            View All
+          </Link>
         </div>
+        <TableWrapper />
       </div>
     </div>
-
-    {/* Table Latest Users */}
-    <div className="flex flex-col justify-center w-full py-5 px-4 lg:px-0  max-w-[90rem] mx-auto gap-3">
-      <div className="flex  flex-wrap justify-between">
-        <h3 className="text-center text-xl font-semibold">Latest Users</h3>
-        <Link
-          href="/accounts"
-          as={NextLink}
-          color="primary"
-          className="cursor-pointer"
-        >
-          View All
-        </Link>
-      </div>
-      <TableWrapper />
-    </div>
-  </div>
-);
+    );
