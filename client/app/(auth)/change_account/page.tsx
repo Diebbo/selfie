bc1efb526687f72f5a96b01507d88e0f4f92114d
Lele <emanuele.2003@yahoo.it>
2024-09-28 17:37:12 +0200

    change user check when already signed in

---
+++ client/app/(auth)/change_account/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ChangeAccountPage } from "@/components/auth/change_account";
+
+const changeaccount = () => {
+  return <ChangeAccountPage />;
+};
+
+export default changeaccount;
