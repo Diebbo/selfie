600e1d78704ecb4d5157851269daf69cc03f19bd
Diebbo <diego.barbieri03@libero.it>
2024-09-29 11:21:09 +0200

    fix verification on client side

--- client/components/auth/emailverification.tsx
+++ client/components/auth/emailverification.tsx
@@ -2,33 +2,37 @@
 
 import { useState, useEffect } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
-import { deleteAuthCookie, verification } from '@/actions/auth.action';
+import { deleteAuthCookie } from '@/actions/auth.action';
+import getBaseUrl from '@/config/proxy';
 
 export function EmailVerificationPage() {
   const [verificationStatus, setVerificationStatus] = useState<string>('Verifica in corso...');
+  const [isVerified, setIsVerified] = useState<boolean>(false);
   const searchParams = useSearchParams();
   const emailToken = searchParams.get('emailToken');
   const router = useRouter();
 
-  useEffect(() => {
-    const verifyEmail = async () => {
-      if (emailToken) {
-        try { 
-          const response = await verification(emailToken);
-          if (response.status === 200) {
-            setVerificationStatus('Verification successful! Click here to go to the login page.');
-          } else {
-            setVerificationStatus('Error during email verification. Invalid token.');
-          }
-        } catch (error) {
-          setVerificationStatus('Error during email verification. Invalid token.');
-        }
-      } else {
-        setVerificationStatus('Verification token missing.');
-      }
-    };
+  const handleVerification = () => {
+    const path = `${getBaseUrl()}/api/auth/verifyemail?emailToken=${emailToken}`;
 
-    verifyEmail();
+    fetch(path, {
+      method: 'PATCH',
+    }).then((res) => {
+      if (!res.ok) {
+        setVerificationStatus('Verifica email fallita. Token non valido.');
+        return;
+      }
+      setVerificationStatus('Verifica email riuscita! Clicca qui per andare alla pagina di login.');
+      setIsVerified(true);
+    }).catch(() => {
+      setVerificationStatus('Verifica email fallita. Token non valido.');
+    });
+  }
+
+  useEffect(() => {
+    if (emailToken) {
+      handleVerification();
+    }
   }, [emailToken]);
 
 
@@ -40,19 +44,19 @@ export function EmailVerificationPage() {
   return (
     <div className="container mx-auto mt-10 text-center">
       <h1 className="text-2xl font-bold mb-4">Verifica Email</h1>
-      <p className={`text-lg ${verificationStatus.includes('successful') ? 'text-green-600' : 'text-red-600'}`}>
+      <p className={`text-lg ${verificationStatus.includes('riuscita') ? 'text-green-600' : 'text-red-600'}`}>
         {verificationStatus}
       </p>
-      {verificationStatus.includes('successful') && (
+      {verificationStatus.includes('riuscita') && (
         <button
           onClick={handleLoginRedirect}
           className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
         >
-          Go to login page
+          Vai alla pagina di login
         </button>
       )}
     </div>
   );
-};
+}
 
 export default EmailVerificationPage;
