c7fec63073c7af03cdd8ed3506f6f07467792674
Diebbo <diego.barbieri03@libero.it>
2024-09-30 15:56:31 +0200

    solito middleware buggato

--- client/middleware.ts
+++ client/middleware.ts
@@ -3,14 +3,14 @@ import { cookies } from 'next/headers'
 import { isVerified } from './actions/auth.action'
 
 // 1. Specify protected and public routes
-const protectedRoutes = ['/dashboard', '/', '/timemachine']
-const publicRoutes = ['/login', '/register', '/verifyemail', '/verification']
+// const protectedRoutes = ['/dashboard', '/', '/timemachine']
+const publicRoutes = ['/login', '/register', '/verifyemail', '/verification', '/reset-login', '/change_account']
 
 export default async function middleware(req: NextRequest) {
   // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname
-  const isProtectedRoute = protectedRoutes.includes(path)
   const isPublicRoute = publicRoutes.includes(path)
+  const isProtectedRoute = !isPublicRoute
   
   let cookie;
   // 3. Decrypt the session from the cookie
@@ -60,7 +60,10 @@ export default async function middleware(req: NextRequest) {
     }
     
   } catch (error) {
-    // token is invalid
+    // token is invalid 
+    if (path === '/login' || path === '/register') { // forward without redirects
+      return NextResponse.next()
+    }
     return NextResponse.redirect(new URL('/login', req.nextUrl))
 
   }
