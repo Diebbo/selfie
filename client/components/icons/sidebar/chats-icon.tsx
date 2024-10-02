14a33ad21819c7db36d8012389c4c0b417d86e88
Diebbo <diego.barbieri03@libero.it>
2024-10-02 19:27:14 +0200

    chats icons + middleware fix AGAIN

---
+++ client/components/icons/sidebar/chats-icon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ChatsIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="fill-default-400"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M30 1.5c-16.542 0-30 12.112-30 27 0 5.205 1.647 10.246 4.768 14.604-0.591 6.537-2.175 11.39-4.475 13.689-0.304 0.304-0.38 0.769-0.188 1.153C0.276 58.289 0.625 58.5 1 58.5c0.046 0 0.093-0.003 0.14-0.01 0.405-0.057 9.813-1.412 16.617-5.338C21.622 54.711 25.738 55.5 30 55.5c16.542 0 30-12.112 30-27S46.542 1.5 30 1.5z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export default ChatsIcon;
