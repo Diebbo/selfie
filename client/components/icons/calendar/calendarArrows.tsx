082b2e99b6df179e3af18b7f10427e165be780a1
Jabbar03 <ayache.omar@gmail.com>
2024-10-04 19:02:13 +0200

    new ActionButton and chores on Calendar

---
+++ client/components/icons/calendar/calendarArrows.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const leftArrowIcon = () => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    fill="none" 
+    viewBox="0 0 24 24" 
+    stroke-width="1.5" 
+    stroke="currentColor" 
+    class="size-6"
+  >
+    <path   
+      stroke-linecap="round"  
+      stroke-linejoin="round" 
+      d="M15.75 19.5 8.25 12l7.5-7.5" 
+    />
+  </svg>
+);
+
+export const rightArrowIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+      <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+    </svg>
+);
