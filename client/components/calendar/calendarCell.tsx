d8bc86ba4d512c9a31a74c3f1344e8671e01e55c
Lele <emanuele.2003@yahoo.it>
2024-10-15 15:24:09 +0200

    tooltip on event button

--- client/components/calendar/calendarCell.tsx
+++ client/components/calendar/calendarCell.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-import React from "react";
-import { Card, CardBody, Badge } from "@nextui-org/react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardBody,
+  Tooltip,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
 import { SelfieEvent } from "@/helpers/types";
 
 const areSameDay = (date1: Date, date2: Date): boolean => {
@@ -56,6 +67,13 @@ const CalendarCell: React.FC<CalendarCellProps> = ({
 }) => {
   const cellDate = new Date(date.getFullYear(), date.getMonth(), day);
   const safeEvents = Array.isArray(events) ? events : [];
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedEvent, setSelectedEvent] = useState<SelfieEvent | null>(null);
+
+  const handleEventClick = (event: SelfieEvent) => {
+    setSelectedEvent(event);
+    onOpen();
+  };
 
   return (
     <Card>
@@ -65,10 +83,81 @@ const CalendarCell: React.FC<CalendarCellProps> = ({
         >
           {day}
         </div>
-        <div className="mt-1 space-y-1 overflow-hidden ">
-          {showEvents(safeEvents, cellDate)}
+        <div className="mt-1 space-y-1 overflow-hidden">
+          {getEventsByDay(safeEvents, cellDate).map((event, index) => (
+            <Tooltip
+              key={index}
+              placement="bottom"
+              closeDelay={0}
+              showArrow={true}
+              classNames={{ base: "pointer-events-none" }}
+              content={
+                <div>
+                  <p>
+                    <strong>Description:</strong>{" "}
+                    {event.description || "No description"}
+                  </p>
+                  <p>
+                    <strong>Location 📍:</strong>{" "}
+                    {event.location || "No location specified"}
+                  </p>
+                </div>
+              }
+            >
+              <button
+                onClick={() => handleEventClick(event)}
+                className="rounded-[100px] py-2 px-4 border-1 border-black bg-slate-700 text-left text-white w-full overflow-hidden truncate dark:hover:border-1 dark:hover:border-white"
+              >
+                {event.title}
+              </button>
+            </Tooltip>
+          ))}
         </div>
       </CardBody>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                {selectedEvent?.title}
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  <strong>Description:</strong> {selectedEvent?.description}
+                </p>
+                <p>
+                  <strong>Start:</strong>{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  }).format(new Date(selectedEvent?.dtstart as Date))}
+                </p>
+                <p>
+                  <strong>End:</strong>{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  }).format(new Date(selectedEvent?.dtend as Date))}
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </Card>
   );
 };
