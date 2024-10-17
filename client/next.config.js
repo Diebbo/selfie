f4c93bc7bdb7755d5ded4b9c9ba63e8e453067cf
Lele <emanuele.2003@yahoo.it>
2024-10-17 18:09:46 +0200

    deploy config js

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 // next.config.js
 module.exports = {
+  reactStrictMode: true,
+  distDir: "nextbuild",
   async rewrites() {
     return [
       {
-        source: '/api/:slug*',
-        destination: 'http://localhost:3001/api/:slug*',
+        source: "/api/:slug*",
+        destination: "http://localhost:3001/api/:slug*",
       },
     ];
   },
--- server/server.js
+++ server/server.js
@@ -1,45 +1,61 @@
-import { createApp } from './src/app.js';
-import { config } from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { createDataBase } from './src/database.js';
-import { fork } from 'child_process';
+import { createApp } from "./src/app.js";
+import { config } from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createDataBase } from "./src/database.js";
+import { fork } from "child_process";
+import next from "next";
 
-config();
+const result = config();
+if (result.error) {
+  config({ path: "/webapp/.env" });
+}
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV === "production") {
+  const dev = false;
+  const nextApp = next({ dev, dir: "/webapp", distDir: "nextbuild" });
+  const handle = nextApp.getRequestHandler();
+  try {
+    await nextApp.prepare();
+  } catch (error) {
+    console.log("Error starting next.js:", error);
+    exit(1);
+  }
+}
 
 // connect to the database
-try{
-    var db = await createDataBase();
-    console.log('server.js: connected to database');
-}catch(err){
-    console.log(err);
-    exit(1);
+try {
+  var db = await createDataBase();
+  console.log("server.js: connected to database");
+} catch (err) {
+  console.log(err);
+  exit(1);
 }
 
 // start the notification worker
-try {
-    const notificationWorker = fork(path.join(__dirname, 'notificationWorker.js'));
-    notificationWorker.send({ message: 'Notification worker started' });
-    notificationWorker.on('message', (message) => {
-        console.log('Notification Worker:', message);
-    });
+/*try {
+  const notificationWorker = fork(
+    path.join(__dirname, "notificationWorker.js"),
+  );
+  notificationWorker.send({ message: "Notification worker started" });
+  notificationWorker.on("message", (message) => {
+    console.log("Notification Worker:", message);
+  });
 } catch (error) {
-    console.log('Error starting notification worker:', error);
-    exit(1);
+  console.log("Error starting notification worker:", error);
+  exit(1);
 }
-
-console.log('server.js: createApp');
+*/
+console.log("server.js: createApp");
 const app = createApp({
-    dirpath: __dirname,
-    database: db,
+  dirpath: __dirname,
+  database: db,
 });
 
 app.listen(PORT, function () {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
-
