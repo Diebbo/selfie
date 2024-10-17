1aa9733da237a911999a9dc6c935b412f2584e43
Lele <emanuele.2003@yahoo.it>
2024-10-17 16:29:20 +0200

    fixed middleware redirect loop on register

--- client/components/auth/emailverification.tsx
+++ client/components/auth/emailverification.tsx
@@ -1,53 +1,62 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { deleteAuthCookie } from '@/actions/auth.action';
-import getBaseUrl from '@/config/proxy';
+import { useState, useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { deleteAuthCookie } from "@/actions/auth.action";
+import getBaseUrl from "@/config/proxy";
 
 export function EmailVerificationPage() {
-  const [verificationStatus, setVerificationStatus] = useState<string>('Verifica in corso...');
+  const [verificationStatus, setVerificationStatus] = useState<string>(
+    "Verifica in corso...",
+  );
   const [isVerified, setIsVerified] = useState<boolean>(false);
   const searchParams = useSearchParams();
-  const emailToken = searchParams.get('emailToken');
+  const emailToken = searchParams.get("emailToken");
   const router = useRouter();
 
   const handleVerification = () => {
     const path = `${getBaseUrl()}/api/auth/verifyemail?emailToken=${emailToken}`;
 
     fetch(path, {
-      method: 'PATCH',
-    }).then((res) => {
-      if (!res.ok) {
-        setVerificationStatus('Verifica email fallita. Token non valido.');
-        return;
-      }
-      setVerificationStatus('Verifica email riuscita! Clicca qui per andare alla pagina di login.');
-      setIsVerified(true);
-    }).catch(() => {
-      setVerificationStatus('Verifica email fallita. Token non valido.');
-    });
-  }
+      method: "PATCH",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          setVerificationStatus("Verifica email fallita. Token non valido.");
+          return;
+        }
+        setVerificationStatus(
+          "Verifica email riuscita! Clicca qui per andare alla pagina di login.",
+        );
+        setIsVerified(true);
+      })
+      .catch(() => {
+        setVerificationStatus("Verifica email fallita. Token non valido.");
+      });
+  };
 
   useEffect(() => {
     if (emailToken) {
       handleVerification();
+    } else {
+      setVerificationStatus("Token non inserito");
     }
   }, [emailToken]);
 
-
   const handleLoginRedirect = async () => {
     await deleteAuthCookie();
-    router.push('/login');
+    router.push("/login");
   };
 
   return (
     <div className="container mx-auto mt-10 text-center">
       <h1 className="text-2xl font-bold mb-4">Verifica Email</h1>
-      <p className={`text-lg ${verificationStatus.includes('riuscita') ? 'text-green-600' : 'text-red-600'}`}>
+      <p
+        className={`text-lg ${verificationStatus.includes("riuscita") ? "text-green-600" : "text-red-600"}`}
+      >
         {verificationStatus}
       </p>
-      {verificationStatus.includes('riuscita') && (
+      {verificationStatus.includes("riuscita") && (
         <button
           onClick={handleLoginRedirect}
           className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
