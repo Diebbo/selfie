01623542a69f194c99bffbfa48e649a1a4eb32e5
Lele <emanuele.2003@yahoo.it>
2024-10-18 19:27:51 +0200

    events cards in dashboard

--- client/actions/user.ts
+++ client/actions/user.ts
@@ -1,31 +1,56 @@
-'use server';
+"use server";
 
-import { cookies } from 'next/headers';
-import getBaseUrl from '@/config/proxy';
-import { Person } from '@/helpers/types';
+import { cookies } from "next/headers";
+import getBaseUrl from "@/config/proxy";
+import { Person, SelfieEvent } from "@/helpers/types";
 
 async function getUser(): Promise<Person> {
   const cookieStore = cookies();
-  const token = cookieStore.get('token')?.value;
+  const token = cookieStore.get("token")?.value;
 
   if (!token) {
-    throw new Error('Not authenticated');
+    throw new Error("Not authenticated");
   }
 
   const response = await fetch(`${getBaseUrl()}/api/users/id`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      'Cookie': `token=${token.toString()}`,
+      "Content-Type": "application/json",
+      Cookie: `token=${token.toString()}`,
     },
   });
 
   if (response.status !== 200) {
     const errorText = await response.text();
-    throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
+    throw new Error(
+      `HTTP error! status: ${response.status}, message: ${errorText}`,
+    );
   }
 
-  return await response.json();
+  const userData = await response.json();
+
+  const person: Person = {
+    _id: userData._id as string,
+    avatar: "",
+    friends: userData.friends as Person[],
+    events: {
+      created: userData.events as SelfieEvent[],
+      participating: userData.participatingEvents as SelfieEvent[],
+    },
+    username: userData.username as string,
+    password: userData.password as string,
+    name: userData.name as string,
+    surname: userData.surname as string,
+    email: userData.email as string,
+    birthDate: new Date(userData.birthDate as string),
+    address: userData.address as string,
+    city: userData.city as string,
+    state: userData.state as string,
+    zip: userData.zip as string,
+    country: userData.country as string,
+  };
+
+  return person;
 }
 
 export { getUser };
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import cookieJwtAuth from './middleware/cookieJwtAuth.js';
+import express from "express";
+import cookieJwtAuth from "./middleware/cookieJwtAuth.js";
 
 export default function createUserRouter(db) {
   const router = express.Router();
 
-  router.get('/id', cookieJwtAuth, async (req, res) => {
+  router.get("/id", cookieJwtAuth, async (req, res) => {
     try {
       const dbuser = (await db.userService.getById(req.user._id)).toObject();
-      const events = await db.getEvents(req.user._id);
+      // const events = await db.getEvents(req.user._id);
       const friends = await db.friendService.get(req.user._id);
 
-      const user = { ...dbuser, events, friends };
+      const user = { ...dbuser, friends };
 
       return res.status(200).json(user);
     } catch (error) {
-      return res.status(404).json({message: error.message});
+      return res.status(404).json({ message: error.message });
     }
   });
 
