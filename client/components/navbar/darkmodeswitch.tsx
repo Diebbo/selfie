5a77e4568a518209e1e13393bb454de9a97bedee
Lele <emanuele.2003@yahoo.it>
2024-10-19 17:09:30 +0200

    better navbar with realTime Clock

--- client/components/navbar/darkmodeswitch.tsx
+++ client/components/navbar/darkmodeswitch.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { useTheme as useNextTheme } from "next-themes";
 import { Switch } from "@nextui-org/react";
+import { MoonIcon } from "../icons/MoonIcon";
+import { SunIcon } from "../icons/SunIcon";
 
 export const DarkModeSwitch = () => {
   const { setTheme, resolvedTheme } = useNextTheme();
   return (
     <Switch
+      color="secondary"
       isSelected={resolvedTheme === "dark" ? true : false}
       onValueChange={(e) => setTheme(e ? "dark" : "light")}
-    />
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <SunIcon className={className} />
+        ) : (
+          <MoonIcon className={className} />
+        )
+      }
+    >
+      {resolvedTheme === "dark" ? "Dark" : "Light"}
+    </Switch>
   );
 };
