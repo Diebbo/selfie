c6e2baefcdcbc6b83b584ed916910c2550e14c91
Diebbo <diego.barbieri03@libero.it>
2024-10-27 22:29:10 +0100

    dark mode on projects

--- client/components/projects/project-modal.mjs
+++ client/components/projects/project-modal.mjs
@@ -22,64 +22,30 @@ class Modal extends HTMLElement {
     }
   }
 
+  setupStyle() {
+    const style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.href = '/style/project-o.css';
+    this.shadowRoot.appendChild(style);
+  }
+    
   setupModal() {
-    const style = document.createElement('style');
-    style.textContent = `
-      .modal {
-        display: none;
-        position: fixed;
-        z-index: 1;
-        left: 0;
-        top: 0;
-        border-radius: 15px;
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-        background-color: rgba(0,0,0,0.4);
-      }
-      .modal-content {
-        background-color: #fefefe;
-        margin: 15% auto;
-        padding: 20px;
-        border: 1px solid #888;
-        width: 80%;
-        max-width: 500px;
-        border-radius: 15px;
-      }
-      .close {
-        color: #aaa;
-        float: right;
-        font-size: 28px;
-        font-weight: bold;
-        cursor: pointer;
-      }
-      .close:hover,
-      .close:focus {
-        color: black;
-        text-decoration: none;
-        cursor: pointer;
-      }
-      .text-error {
-        color: red;
-      }
-      h2 {
-        color: #333;
-      }
-    `;
+    this.setupStyle();
 
     const modal = document.createElement('div');
     modal.classList.add('modal');
     modal.innerHTML = `
       <div class="modal-content">
-        <span class="close">&times;</span>
-        <h2 id="modalTitle"></h2>
-        <p id="modalError" class="text-error"></p>
+        <div class="headers">
+            <span class="close">&times;</span>
+            <h2 id="modalTitle"></h2>
+            <p id="modalError" class="text-error"></p>
+        </div>
         <slot></slot>
       </div>
     `;
 
     this.shadowRoot.appendChild(modal);
-    this.shadowRoot.appendChild(style);
 
     this.modal = this.shadowRoot.querySelector('.modal');
     this.closeBtn = this.shadowRoot.querySelector('.close');
