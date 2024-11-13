9f8aa14607401c38867a383c7a9a7392fc5ae863
Jabbar03 <ayache.omar@gmail.com>
2024-11-13 01:20:33 +0100

    WIP: resources in the eventAdder Button

--- client/app/(app)/calendar/page.tsx
+++ client/app/(app)/calendar/page.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import CalendarPage from "@/components/calendar/calendar";
 import { getUser } from "@/actions/user";
 import { getCurrentTime } from "@/actions/setTime";
-import { Person, People, ProjectModel } from "@/helpers/types";
+import { Person, People, ProjectModel, ResourceModel } from "@/helpers/types";
 import { getFriends } from "@/actions/friends";
 import getProjects from "@/actions/projects"
+import { getResource } from "@/actions/events";
 
 const Page = async () => {
   try {
-    const [dbDate, friends, user, projects]: [Date, People, Person, ProjectModel[]] = await Promise.all([
+    const [dbDate, friends, user, projects, resource]: [Date, People, Person, ProjectModel[], ResourceModel[]] = await Promise.all([
       getCurrentTime(),
       getFriends(),
       getUser(),
       getProjects(),
+      getResource()
     ]);
     return <CalendarPage
       createdEvents={user.events.created}
@@ -20,6 +22,7 @@ const Page = async () => {
       dbdate={new Date(dbDate)}
       friends={friends}
       projects={projects}
+      resource={resource}
     />;
   } catch (error) {
     console.log(error);
