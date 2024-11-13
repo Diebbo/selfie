a47abdc2fce1202d8c1a94b0a0dddfb18196d519
Diebbo <diego.barbieri03@libero.it>
2024-11-13 14:33:38 +0100

    fix conflixcts with calendar

--- client/app/(app)/calendar/page.tsx
+++ client/app/(app)/calendar/page.tsx
@@ -7,16 +7,22 @@ import { getFriends } from "@/actions/friends";
 import { getTasks } from "@/actions/tasks";
 import getProjects from "@/actions/projects"
 import { TaskMutiResponse } from "@/helpers/api-types";
+import { showError } from "@/helpers/error-checker";
 
 const Page = async () => {
   try {
-    const [dbDate, friends, user, projects, tasks]: [Date, People, Person, ProjectModel[], TaskMutiResponse] = await Promise.all([
+    const [dbDate, friends, user, projects, tasks]: [Date, People, Person | Error, ProjectModel[], TaskMutiResponse] = await Promise.all([
       getCurrentTime(),
       getFriends(),
       getUser(),
       getProjects(),
       getTasks(),
     ]);
+
+    if (user instanceof Error) {
+      return showError(user);
+    }
+
     return <CalendarPage
       createdEvents={user.events.created}
       participatingEvents={user.events.participating}
@@ -26,7 +32,7 @@ const Page = async () => {
       projects={projects}
     />;
   } catch (error) {
-    console.log(error);
+    return showError(error);
   }
 };
 
