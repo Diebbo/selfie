8918f4f8f320c5823eb153d8d86a69218a2b0bce
Diebbo <diego.barbieri03@libero.it>
2024-11-14 18:12:22 +0100

    week show activity project

--- client/components/calendar/weekViewGrid.tsx
+++ client/components/calendar/weekViewGrid.tsx
@@ -2,15 +2,16 @@
 
 import React, { useState } from 'react';
 import { useRouter } from "next/navigation";
-import { SelfieEvent, ProjectModel, TaskModel } from "@/helpers/types";
+import { SelfieEvent, ProjectModel, TaskModel, ProjectTaskModel } from "@/helpers/types";
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Chip, Input } from "@nextui-org/react";
 import { useTime } from "../contexts/TimeContext";
 
 interface CombinedAppointment {
-  type: 'event' | 'project' | 'task';
+  type: 'event' | 'project' | 'task' | 'project-task';
   event?: SelfieEvent;
   project?: ProjectModel;
   task?: TaskModel;
+  projectTask?: ProjectTaskModel;
 }
 
 const generateHours = () => {
@@ -21,6 +22,12 @@ const generateHours = () => {
   }
   return hours;
 };
+
+function isSameDay(date1: Date, date2: Date) {
+  return date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate();
+}
 
 export const WeekViewGrid: React.FC<{
   date: Date,
@@ -53,19 +60,37 @@ export const WeekViewGrid: React.FC<{
 
     // Add project tasks
     projects.forEach(project => {
-      project.activities.forEach(task => {
-        const taskDate = new Date(task.dueDate);
-        if (taskDate.toDateString() === date.toDateString()) {
+      if (isSameDay(date, new Date(project.deadline))) {
           appointments.push({
             type: 'project',
             project: {
               ...project,
-              activities: [task] // Include only the relevant task
             }
           });
+      }
+      project.activities.forEach(task => {
+        const taskDate = new Date(task.dueDate);
+        if (isSameDay(date, taskDate)) {
+          appointments.push({
+            type: 'project-task',
+            projectTask: task,
+            project: project
+          });
         }
+        task.subTasks?.forEach(subTask => {
+          if (isSameDay(date, new Date(subTask.dueDate))) {
+            appointments.push({
+              type: 'project-task',
+              projectTask: subTask,
+              project: project
+            });
+          }
+        });
       });
     });
+
+    console.log('projects', appointments.filter(app => app.type === 'project'));
+    console.log(appointments.filter(app => app.type === 'project-task'));
 
     // Add individual tasks
     if (tasks) {
@@ -83,13 +108,28 @@ export const WeekViewGrid: React.FC<{
     return appointments;
   };
 
+  const getDayOfAppointment = (appointment: CombinedAppointment) => {
+    let appointmentDate;
+    switch (appointment.type) {
+      case 'event':
+        appointmentDate = new Date(appointment.event!.dtstart);
+        break;
+      case 'project':
+        appointmentDate = new Date(appointment.project!.deadline);
+        break;
+      case 'task':
+        appointmentDate = new Date(appointment.task!.dueDate);
+        break;
+      case 'project-task':
+        appointmentDate = new Date(appointment.projectTask!.dueDate);
+        break;
+    }
+    return appointmentDate;
+  }
+
   const handleOpenAppointmentModal = (hourIndex: number) => {
     const appointmentsForHour = getDayAppointments().filter(appointment => {
-      const appointmentDate = appointment.type === 'event'
-        ? new Date(appointment.event!.dtstart)
-        : appointment.type === 'project'
-          ? new Date(appointment.project!.activities[0].dueDate)
-          : new Date(appointment.task!.dueDate);
+      const appointmentDate = getDayOfAppointment(appointment);
       return appointmentDate.getHours() === hourIndex;
     });
 
@@ -99,11 +139,7 @@ export const WeekViewGrid: React.FC<{
 
   const getAppointmentsForHour = (hourIndex: number): CombinedAppointment[] => {
     return getDayAppointments().filter(appointment => {
-      const appointmentDate = appointment.type === 'event'
-        ? new Date(appointment.event!.dtstart)
-        : appointment.type === 'project'
-          ? new Date(appointment.project!.activities[0].dueDate)
-          : new Date(appointment.task!.dueDate);
+      const appointmentDate = getDayOfAppointment(appointment);
       return appointmentDate.getHours() === hourIndex;
     });
   };
@@ -112,7 +148,7 @@ export const WeekViewGrid: React.FC<{
     const count = appointments.length;
     const hasEvents = appointments.some(a => a.type === 'event');
     const hasProjects = appointments.some(a => a.type === 'project');
-    const hasTasks = appointments.some(a => a.type === 'task');
+    const hasTasks = appointments.some(a => a.type === 'task' || a.type === 'project-task');
 
     function handleColor() {
       if (hasProjects && hasTasks && hasEvents) {
@@ -133,11 +169,7 @@ export const WeekViewGrid: React.FC<{
           ${handleColor}`}
 
         aria-label={count > 2 ? "tooManyAppoinments" : ""}
-        onPress={() => handleOpenAppointmentModal(parseInt(appointments[0].type === 'event'
-          ? new Date(appointments[0].event!.dtstart).getHours().toString()
-          : appointments[0].type === 'project'
-            ? new Date(appointments[0].project!.activities[0].dueDate).getHours().toString()
-            : new Date(appointments[0].task!.dueDate).getHours().toString()))}
+        onPress={() => handleOpenAppointmentModal(getDayOfAppointment(appointments[0]).getHours())}
       >
         {(hasEvents || hasTasks || hasProjects) &&
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M600-80v-80h160v-400H200v160h-80v-320q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H600ZM320 0l-56-56 103-104H40v-80h327L264-344l56-56 200 200L320 0ZM200-640h560v-80H200v80Zm0 0v-80 80Z" /></svg>
@@ -145,6 +177,18 @@ export const WeekViewGrid: React.FC<{
       </Button>
     );
   };
+  const handleRedirect = (appointment: CombinedAppointment) => {
+    switch (appointment.type) {
+      case 'event':
+        return `/events/${appointment.event!._id}`;
+      case 'project':
+        return `/projects/${appointment.project!._id}`;
+      case 'task':
+        return `/task`;
+      case 'project-task':
+        return `/projects/${appointment.project?._id}`;
+    }
+  }
 
   // Helper function to render participants
   const renderParticipants = (appointment: CombinedAppointment) => {
@@ -166,6 +210,19 @@ export const WeekViewGrid: React.FC<{
       </div>
     );
   };
+
+  const getAppointmentName = (appointment: CombinedAppointment) => {
+    switch (appointment.type) {
+      case 'event':
+        return appointment.event!.title;
+      case 'project':
+        return appointment.project!.title;
+      case 'task':
+        return appointment.task!.name;
+      case 'project-task':
+        return appointment.projectTask!.title;
+    }
+  }
 
   return (
     <div className="overflow-y-auto scrollbar-hide max-h-[calc(85vh)]">
@@ -196,13 +253,7 @@ export const WeekViewGrid: React.FC<{
               <ModalHeader className="flex flex-col gap-1">
                 {selectedHourAppointments.length > 0 && (
                   <span>
-                    Appuntamenti alle {new Date(
-                      selectedHourAppointments[0].type === 'event'
-                        ? selectedHourAppointments[0].event!.dtstart
-                        : selectedHourAppointments[0].type === 'project'
-                          ? selectedHourAppointments[0].project!.activities[0].dueDate
-                          : selectedHourAppointments[0].task!.dueDate
-                    ).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                    Appuntamenti alle {getDayOfAppointment(selectedHourAppointments[0]).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
                   </span>
                 )}
               </ModalHeader>
@@ -213,11 +264,7 @@ export const WeekViewGrid: React.FC<{
                       <Button
                         className="mb-2 p-[2rem] border-1 border-default hover:border-secondary w-full"
                         onClick={() => router.push(
-                          appointment.type === 'event'
-                            ? `/calendar/${appointment.event!._id}`
-                            : appointment.type === 'project'
-                              ? `/projects/${appointment.project!._id}`
-                              : `/task`
+                          handleRedirect(appointment)
                         )}
                       >
                         <div className="flex flex-row items-start w-full">
@@ -227,21 +274,15 @@ export const WeekViewGrid: React.FC<{
                               color={appointment.type === 'event' ? 'primary' : appointment.type === 'project' ? 'warning' : 'secondary'}
                               variant="flat"
                             >
-                              {appointment.type === 'event' ? 'Evento' : appointment.type === 'project' ? 'Progetto' : 'Task'}
+                              {appointment.type.charAt(0).toUpperCase().concat(appointment.type.slice(1).replace('-', ' '))}
                             </Chip>
                             <p className="font-semibold">
-                              {appointment.type === 'event'
-                                ? appointment.event!.title
-                                : `${appointment.project!.title} - ${appointment.project!.activities[0].title}`}
+                              {
+                                getAppointmentName(appointment)
+                              }
                             </p>
                             <p className="text-small text-default-500 justify-end">
-                              {new Date(
-                                appointment.type === 'event'
-                                  ? appointment.event!.dtstart
-                                  : appointment.type === 'project'
-                                    ? appointment.project!.activities[0].dueDate
-                                    : appointment.task!.dueDate
-                              ).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                              {getDayOfAppointment(appointment).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
                             </p>
                           </div>
                         </div>
