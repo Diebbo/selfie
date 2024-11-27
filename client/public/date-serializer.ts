19b49a6306ee4233b8b38845dec5a1311289225c
Jabbar03 <ayache.omar@gmail.com>
2024-11-27 13:47:46 +0100

    remove custom serialization

--- client/public/date-serializer.ts
+++
@@ -1,8 +0,0 @@
-export const serializeWithLocalDates = (obj: any) => {
-  return JSON.stringify(obj, (key, value) => {
-    if (value instanceof Date) {
-      return value.toLocaleString('sv', { timeZoneName: 'short' });
-    }
-    return value;
-  });
-}
