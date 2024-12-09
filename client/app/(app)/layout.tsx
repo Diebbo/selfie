c4f6561096c938705792e3029c13234ac83cad40
Lele <emanuele.2003@yahoo.it>
2024-12-09 12:40:18 +0100

    cleaning packages

--- client/app/(app)/layout.tsx
+++ client/app/(app)/layout.tsx
@@ -1,7 +1,6 @@
 "use server";
 import { Layout } from "@/components/layout/layout";
 import "@/styles/globals.css";
-import "leaflet/dist/leaflet.css";
 import { getCurrentTime } from "@/actions/setTime";
 import { ReloadProvider } from "@/components/calendar/contextStore";
 import { TimeProvider } from "@/components/contexts/TimeContext";
