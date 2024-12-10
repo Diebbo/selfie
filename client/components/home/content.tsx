750bfac2e9b2409d24b95cf9a1d9e02e7464f1c9
Diebbo <diego.barbieri03@libero.it>
2024-12-10 22:20:16 +0100

    right responsiveness of home

--- client/components/home/content.tsx
+++ client/components/home/content.tsx
@@ -100,7 +100,7 @@ export const Content = (props: ContentProps): ReactElement => {
       <div className="flex justify-center gap-5 pt-4 px-4 xl:px-3 2xl:px-5 flex-wrap xl:flex-nowrap max-w-[100rem] mx-auto w-full">
         <div className="mt-6 gap-6 flex flex-col w-full">
           {/* Card Section Top */}
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-3">
+          <div className="grid grid-cols-1 l:grid-cols-2 gap-3">
             <div className="flex flex-col border rounded-lg p-4 h-[500px]">
               <div className="flex justify-between items-center mb-4">
                 <div className="flex gap-1">
